ab79ba7705f31f56462ac79c20f5662d75ff88d8
Emil Kirilov <emil.k@zekeng.com>
2019-06-14 12:34:57 +0300

    DT70 variant (initial version, not mapped)

---
+++ variants/DT70_REV1_STM32F030C8/variant.cpp
@@ -0,0 +1,148 @@
+/*
+  Copyright (c) 2011 Arduino.  All right reserved.
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+  See the GNU Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#include "variant.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// Pin number
+const PinName digitalPin[] = {
+  PB_7,  //D0 RX0
+  PB_6,  //D1 TX0
+  PA_1,  //D2
+  PA_7,  //D3/SPI_MOSI
+  PA_8,  //D4 (PWM1)
+  PA_9,  //D5 (PWM2)
+  PA_10, //D6
+  PA_11, //D7
+  PA_12, //D8
+  PA_15, //D9
+  PB_2,  //D10
+  PB_3,  //D11/SPI_SCK
+  PF_7,  //D12/SDA
+  PF_6,  //D13/SCL
+  PA_2,  //D14/TX1
+  PA_3,  //D15/RX1
+  PB_4,  //D16/SPI_MISO
+  PB_5,  //D17
+  PB_8,  //D18
+  PB_9,  //D19
+  PB_10, //D20
+  PB_11, //D21
+  PB_12, //D22
+  PB_13, //D23
+  PB_14, //D24
+  PB_15, //D25
+  PC_13, //D26
+  PC_14, //D27
+  PC_15, //D28
+  PA_0,  //D29/A0
+  PA_4,  //D30/A1
+  PA_5,  //D31/A2
+  PA_6,  //D32/A3
+  PB_0,  //D33/A4
+  PB_1,  //D34/A5
+};
+
+#ifdef __cplusplus
+}
+#endif
+
+// ----------------------------------------------------------------------------
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+  * @brief  System Clock Configuration
+  *         The system Clock is configured as follow :
+  *            System Clock source            = PLL (HSI/2)
+  *            SYSCLK(Hz)                     = 48000000
+  *            HCLK(Hz)                       = 48000000
+  *            AHB Prescaler                  = 1
+  *            APB1 Prescaler                 = 1
+  *            HSI Frequency(Hz)              = 8000000
+  *            PREDIV                         = 1
+  *            PLLMUL                         = 12
+  *            Flash Latency(WS)              = 1
+  * @param  None
+  * @retval None
+  */
+WEAK void SystemClock_Config(void)
+{
+
+	  RCC_OscInitTypeDef RCC_OscInitStruct;
+	  RCC_ClkInitTypeDef RCC_ClkInitStruct;
+	  RCC_PeriphCLKInitTypeDef PeriphClkInit;
+
+	    /**Initializes the CPU, AHB and APB busses clocks
+	    */
+	  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI|RCC_OSCILLATORTYPE_HSI14
+	                              |RCC_OSCILLATORTYPE_LSI;
+	  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+	  RCC_OscInitStruct.HSI14State = RCC_HSI14_ON;
+	  RCC_OscInitStruct.HSICalibrationValue = 16;
+	  RCC_OscInitStruct.HSI14CalibrationValue = 16;
+	  RCC_OscInitStruct.LSIState = RCC_LSI_ON;
+	  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+	  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+	  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL12;
+	  RCC_OscInitStruct.PLL.PREDIV = RCC_PREDIV_DIV1;
+	  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+	  {
+	    _Error_Handler(__FILE__, __LINE__);
+	  }
+
+	    /**Initializes the CPU, AHB and APB busses clocks
+	    */
+	  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+	                              |RCC_CLOCKTYPE_PCLK1;
+	  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+	  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+	  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+
+	  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK)
+	  {
+	    _Error_Handler(__FILE__, __LINE__);
+	  }
+
+	  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USART1;
+	  PeriphClkInit.Usart1ClockSelection = RCC_USART1CLKSOURCE_HSI;
+	  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
+	  {
+	    _Error_Handler(__FILE__, __LINE__);
+	  }
+
+	    /**Configure the Systick interrupt time
+	    */
+	  HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq()/1000);
+
+	    /**Configure the Systick
+	    */
+	  HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK);
+
+	  /* SysTick_IRQn interrupt configuration */
+	  HAL_NVIC_SetPriority(SysTick_IRQn, 0, 0);
+}
+
+#ifdef __cplusplus
+}
+#endif
